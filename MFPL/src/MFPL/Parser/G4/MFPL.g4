4505a00e7dd906f0b957e2a646d0ca5e2fd11172
sdflysha <sdflysha@qq.com>
2016-08-17 14:39:46 +0800

    il hello world

---
+++ MFPL/src/MFPL/Parser/G4/MFPL.g4
@@ -0,0 +1,66 @@
+﻿grammar mfpl;
+
+root
+	: expression;
+	;
+
+expression
+	: NUMBER;
+	;
+
+value
+   : STRING
+   | NUMBER
+   | object
+   | array
+   | 'true'
+   | 'false'
+   | 'null'
+   ;
+
+object
+   : '{' pair (',' pair)* '}'
+   | '{' '}'
+   ;
+
+pair
+   : STRING ':' value
+   ;
+
+array
+   : '[' value (',' value)* ']'
+   | '[' ']'
+   ;
+
+STRING
+   : '"' (ESC | ~ ["\\])* '"'
+   ;
+fragment ESC
+   : '\\' (["\\/bfnrt] | UNICODE)
+   ;
+fragment UNICODE
+   : 'u' HEX HEX HEX HEX
+   ;
+
+NUMBER
+   : '-'? INT '.' [0-9] + EXP? | '-'? INT EXP | '-'? INT
+   ;
+
+fragment INT
+   : '0' | [1-9] [0-9]*
+   ;
+
+fragment EXP
+   : [Ee] [+\-]? INT
+   ;
+
+WS
+   : [ \t\n\r] +    -> skip
+   ;
+
+BlockComment
+	: '/*' .*? '*/' -> skip
+	;
+LineComment
+	: '//' ~[\r\n]* -> skip
+	;
--- MFPL/src/MFPL/Program.cs
+++ MFPL/src/MFPL/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 
 namespace MFPL
@@ -9,7 +11,20 @@ namespace MFPL
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello MFPL");
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("Assembly"), AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule("Module");
+            var typeBuilder = moduleBuilder.DefineType("Type", TypeAttributes.Public | TypeAttributes.Class);
+            var methodBuilder = typeBuilder.DefineMethod("Method", MethodAttributes.Public | MethodAttributes.Static);
+
+            var il = methodBuilder.GetILGenerator();
+            il.Emit(OpCodes.Ldstr, "Hello World");
+            il.Emit(OpCodes.Call, typeof(Console).GetMethod(nameof(Console.WriteLine), new[] { typeof(string) }));
+            il.Emit(OpCodes.Ret);            
+
+            var typeInfo = typeBuilder.CreateTypeInfo();
+            var method = typeInfo.GetMethod(methodBuilder.Name);
+            var action = (Action)method.CreateDelegate(typeof(Action));
+            action();
         }
     }
 }
