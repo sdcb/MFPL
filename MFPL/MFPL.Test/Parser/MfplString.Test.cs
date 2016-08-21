88f7f4679b727b11033540c353f8da4591f9b5cb
flysha <flysha@live.com>
2016-08-21 23:06:25 +0800

    add MfplStringTest

---
+++ MFPL/MFPL.Test/Parser/MfplString.Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using static MFPL.Parser.Utilities.MfplString;
+
+namespace MFPL.Test.Parser
+{
+    public class MfplStringTest
+    {
+		[Theory]
+		[InlineData('\'', '"')]
+		[InlineData('"', '\'')]
+		public void GetEscapedCharTest(char input, char expected)
+		{
+			var ch = Details.GetEscapedChar(input);
+			Assert.Equal(expected, ch.Value);
+		}
+	}
+}
---
+++ MFPL/src/MFPL/Parser/MfplString.cs
@@ -0,0 +1,58 @@
+﻿using MFPL.Functional;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MFPL.Parser.Utilities
+{
+    public static class MfplString
+    {
+		public static Result<string> Escape(string text)
+		{
+			if (string.IsNullOrWhiteSpace(text))
+				return Result.Fail<string>($"string literial must not be empty.");
+
+			if (text.Length <= 2)
+				return Result.Fail<string>($"string literial length must > 2.");
+
+			if (text.First() != text.Last())
+				return Result.Fail<string>($"string literial's first and last char must be the same.");
+
+			var firstChar = text[0];
+			var escapedChar = Details.GetEscapedChar(firstChar);
+			if (escapedChar.IsFailure)
+				return Result.Fail<string>(escapedChar.Error);
+
+			var inner = text.Substring(1, text.Length - 2);
+			return Result.Ok(Details.EscapeNoCheck(inner, escapedChar.Value));
+		}
+
+		public class Details
+		{
+			public static Result<char> GetEscapedChar(char ch)
+			{
+				if (ch == '\'')
+				{
+					return Result.Ok('"');
+				}
+				else if (ch == '"')
+				{
+					return Result.Ok('\'');
+				}
+				else
+				{
+					return Result.Fail<char>($"char {ch} not escappable.");
+				}
+			}
+
+			public static string EscapeNoCheck(string input, char escapeChar)
+			{
+				var regex = new Regex($@"\\([\\\/bfnrt{escapeChar}])");
+				var replacedString = regex.Replace(input, "$1");
+				return replacedString;
+			}
+		}
+    }
+}
