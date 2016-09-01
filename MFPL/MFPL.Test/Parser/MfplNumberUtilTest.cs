42e4a6471af4e99f2a4c2826a095f6665fe7baf2
flysha <flysha@live.com>
2016-09-01 20:38:56 +0800

    move 3 Util to Utilities folder.

--- MFPL/MFPL.Test/Parser/MfplNumberUtilTest.cs
+++ MFPL/MFPL.Test/Parser/MfplNumberUtilTest.cs
@@ -1,4 +1,4 @@
-﻿using MFPL.Parser;
+﻿using MFPL.Parser.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- MFPL/src/MFPL/Parser/Utilities/MfplNumberUtil.cs
+++ MFPL/src/MFPL/Parser/Utilities/MfplNumberUtil.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MFPL.Parser
+namespace MFPL.Parser.Utilities
 {
     public static class MfplNumberUtil
     {
